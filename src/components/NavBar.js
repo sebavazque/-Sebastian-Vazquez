c6a130c63977c92d1320d08021ab357f0d96fcfa
sebavazque <88560731+sebavazque@users.noreply.github.com>
2022-02-23 20:02:50 -0300

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar(){
+    return <header>  
+        <div className="topNav">
+            <nav className="navBar">
+                <a href="#">Inicio</a>
+                <a href="#">Produtos</a>
+                <a href="#">Contacto</a>
+            </nav>
+        </div>
+        <h1 className="title">Title</h1>
+    </header>
+}
+
+export default NavBar
