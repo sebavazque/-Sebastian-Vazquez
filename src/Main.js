33dfefa39074e8c2f7a7b0b8e71ded4b7f02b535
sebavazque <88560731+sebavazque@users.noreply.github.com>
2022-03-28 23:52:37 -0300

    Add files via upload

---
+++ src/Main.js
@@ -0,0 +1,36 @@
+
+import { Link } from "react-router-dom";
+
+
+
+
+
+
+
+const Main = (props) => {
+
+
+    
+
+    return (
+        <main>
+            <div className="mainImage" >
+                <div className="sliderContent">                
+                    <h2 className="title">Bienvenidos a {props.title} </h2>
+                    <p className="text">Somos una empresa de indumentaria fundada en {props.year} </p></div>
+
+            </div>
+
+            <ul className="listaMain">
+                                <li>
+                                    <Link to="/item/remeras">Remeras</Link>
+                                    <Link to="/item/pantalones">Pantalones</Link>
+                                    <Link to="/item/gorros">Gorros</Link>
+                                </li>
+            </ul>
+            <hr />
+        </main>
+    );
+}
+
+export default Main;
